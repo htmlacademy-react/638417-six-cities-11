4b30af81c161200d139b823744003ba26897cb57
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-10-23 15:19:30 +0300

    Qout err fix

--- project/src/pages/property-page/property-page.tsx
+++ project/src/pages/property-page/property-page.tsx
@@ -71,7 +71,7 @@ function PropertyPage({ favoritesCount }: PropertyPageProps): JSX.Element {
                 <span className="property__price-text">&nbsp;night</span>
               </div>
               <div className="property__inside">
-                <h2 className="property__inside-title">What's inside</h2>
+                <h2 className="property__inside-title">What&quot;s inside</h2>
                 <ul className="property__inside-list">
                   <li className="property__inside-item">
                     Wi-Fi
