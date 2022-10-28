60055ebdbe590a5daa3fa73c37bd1eb5b9cb5423
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-10-28 07:12:33 +0300

    3.4 Private Route component

--- project/src/consts.ts
+++ project/src/consts.ts
@@ -2,5 +2,11 @@ export enum AppRoute {
   Main = '/',
   Login = '/login',
   Favorites = '/favorites',
-  Room = '/offer',
+  Room = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
 }
