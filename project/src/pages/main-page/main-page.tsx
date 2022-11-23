d3374a67aa7b4d6f0f111c2641636c85ebbe0987
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-11-23 02:47:25 +0200

    Sort component

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import CardList from '../../components/card-list/card-list';
 import Header from '../../components/header/header';
 import Map from '../../components/map/map';
+import Sort from '../../components/sort/sotr';
 import { Offer, Offers } from '../../types/offer';
 
 type MainPageProps = {
@@ -53,21 +54,7 @@ function MainPage({ favoritesCount, offers }: MainPageProps,): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom">{/*places__options--opened*/}
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <Sort/>
               <div className="cities__places-list places__list tabs__content">
                 <CardList offers={offers} onOfferListItemMouseOver={onOfferListItemMouseOver} onOfferListItemMouseOut={onOfferListItemMouseOut}/>
               </div>
